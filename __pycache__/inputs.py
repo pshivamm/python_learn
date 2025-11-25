563007308dfae5f2872d67c3db29a86219f9c9bc
shivam <kripeshmishra97@gmail.com>
2025-11-25 19:42:37 +0530

    New chnages

---
+++ __pycache__/inputs.py
@@ -0,0 +1,18 @@
+# Inputs sum
+
+a = input("Enter first number:")
+b = input("Enter second number:")
+
+print("Number a is: ", a)
+print("Number b is: ", b)
+
+print("Sum of a and b: ", a + b)
+
+
+a = int(input("Enter first number:"))
+b = int(input("Enter second number:"))
+
+print("Number a is: ", a)
+print("Number b is: ", b)
+
+print("Sum of a and b: ", a + b)
---
+++ types.py
@@ -0,0 +1,17 @@
+# Data types 
+a = 32
+b = "john"
+c = 45.5
+f = type(c)
+
+print(f)
+
+# Convert data types
+
+a = "35"
+b = float(a)
+c = int(a)
+d = str(a)
+f = type(c)
+
+print(f)
